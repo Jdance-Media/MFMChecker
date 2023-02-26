cbde20e0f6ca154ee43ce74cd5d32c4efa406728
Jdance-Media <67397422+Jdance-Media@users.noreply.github.com>
2023-02-25 21:53:42 -0500

    Added the rest of the items

--- MFMChecker.cs
+++ MFMChecker.cs
@@ -18,13 +18,20 @@ namespace MFMChecker
         private void onLoad(int level)
         {
             List<ushort> stupidAssets = new List<ushort>();
-            stupidAssets.Add(47181);
-            stupidAssets.Add(42291);
             stupidAssets.Add(47228);
+            stupidAssets.Add(47356);
+            stupidAssets.Add(47357);
             stupidAssets.Add(47358);
             stupidAssets.Add(47359);
-            stupidAssets.Add(47357);
-            stupidAssets.Add(47356);
+            stupidAssets.Add(47181);
+            stupidAssets.Add(47352);
+            stupidAssets.Add(47353);
+            stupidAssets.Add(47354);
+            stupidAssets.Add(47355);
+            stupidAssets.Add(42291);
+            stupidAssets.Add(47242);
+            stupidAssets.Add(47243);
+
             List<RemovalQueue> removals = new List<RemovalQueue>();
             foreach (BarricadeRegion region in BarricadeManager.regions)
             {
